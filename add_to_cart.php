fab25e42fe38d272cdba84a13fc6ebc6aebaf6c7
leonchinzeye <leonchin499@gmail.com>
2015-03-19 23:20:48 +0800

    added some functions

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,5 +1,5 @@
 <?php
 include "extraphpfunctions.php";
 
-
+// $id = isset($_GET["id"]) ? $_GET["id"] : "";
 ?>
--- extraphpfunctions.php
+++ extraphpfunctions.php
@@ -8,4 +8,10 @@ function debugPrintVar($string, $int) {
 	echo "<script>console.log('".$string.": ".$int."');</script>";
 } 
 
+function searchCartByKey($array, $key, $val) {
+	foreach ($array as $item)
+        if (isset($item[$key]) && $item[$key] == $val)
+            return true;
+    return false;
+}
 ?>
